defb18dbfc022f2d50df1e44b4e8af8ba93f022e
LucaFeltrin14 <lucasf6@al.insper.edu.br>
2023-04-19 12:26:28 -0300

    Anda pros Lado, HTML e CSS

--- funcoes.py
+++ funcoes.py
@@ -11,21 +11,40 @@ class Tela:
         
         self.assets = {
             'imagem_fundo': imagem_fundo,
-            'toshi':imagem_toshi
+            'toshi':imagem_toshi,
+            't0' : 0,
+            'tempo': 0
         }
 
-        self.personagem = Personagem(self.window, [100, 100] , self.assets)
+        self.personagem = Personagem(self.window, [230, 600] , self.assets)
     
     def atualiza_estado(self):
+        t1 = pygame.time.get_ticks()
+        tempo = t1- self.assets['t0'] if self.assets['t0']>=0 else t1
+        self.assets['t0'] = t1
+        self.assets['tempo'] = tempo
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return False
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_a:
-                self.personagem.velocidade[0] +=10
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    self.personagem.velocidade[0] -=1
+                if event.key == pygame.K_d:
+                    self.personagem.velocidade[0] +=1
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_a:
+                    self.personagem.velocidade[0] +=1
+                if event.key == pygame.K_d:
+                    self.personagem.velocidade[0] -=1
 
+        
+        
 
         self.personagem.update()
-        
+
+        self.personagem.muda_posicao()
+
         return True
 
     def desenha(self):
@@ -38,8 +57,6 @@ class Tela:
             self.desenha()
 
 
-# toshi = Tela()
-
 class Personagem:
     def __init__(self, window, pos,assets):
         self.vida = 3
@@ -47,11 +64,17 @@ class Personagem:
         self.rect = pygame.Rect(pos[0], pos[1], 80, 100)
         self.window = window
         self.assets = assets
-
+        self
     
     def desenha(self):
         self.window.blit(self.assets['toshi'],(self.rect.x, self.rect.y))
     
     def update(self):
-        self.rect.x += self.velocidade[0]
-        self.rect.y += self.velocidade[1]
+        self.rect.x += self.velocidade[0]*self.assets['tempo']
+        self.rect.y += self.velocidade[1]*self.assets['tempo']
+
+    def muda_posicao(self):
+        if self.rect.x >= 540:
+            self.rect.x = 1
+        if self.rect.x <= 0:
+            self.rect.x = 530
