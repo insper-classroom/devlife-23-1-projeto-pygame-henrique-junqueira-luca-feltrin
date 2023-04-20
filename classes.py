20f109f262792548775d4b8dc8656749672b8af0
LucaFeltrin14 <lucasf6@al.insper.edu.br>
2023-04-20 15:35:56 -0300

    Persongaem PULA vitoria brasil

--- classes.py
+++ classes.py
@@ -27,10 +27,11 @@ class Tela:
             'toshi':imagem_toshi,
             't0' : 0,
             'tempo': 0,
-            'gravidade': 0
+            'gravidade': 1,
+            'pulando': False
         }
 
-        self.personagem = Personagem(self.window, [230, 600] , self.assets)
+        self.personagem = Personagem(self.window, [230, 400] , self.assets)
     
     def atualiza_estado(self):
         pygame.time.Clock().tick(120)
@@ -52,23 +53,23 @@ class Tela:
                     self.personagem.velocidade[0] -=1000
                 if event.key == pygame.K_d:
                     self.personagem.velocidade[0] +=1000
-                if event.key == pygame.K_w:
-                    self.personagem.velocidade[1] -=1000
-                if event.key == pygame.K_s:
-                    self.personagem.velocidade[1] +=1000
+                if event.key == pygame.K_SPACE:
+                    self.assets['pulando'] = True
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_a:
                     self.personagem.velocidade[0] +=1000
                 if event.key == pygame.K_d:
                     self.personagem.velocidade[0] -=1000
-                if event.key == pygame.K_w:
-                    self.personagem.velocidade[1] +=1000
-                if event.key == pygame.K_s:
-                    self.personagem.velocidade[1] -=1000
             if event.type == pygame.QUIT:
                 return False
-        
-        
+       
+        if self.assets['pulando']:
+            self.personagem.rect.y -= self.personagem.y_vel
+            self.personagem.y_vel -= self.assets['gravidade']
+            if self.personagem.y_vel < -self.personagem.jump_alt:
+                self.assets['pulando'] = False
+                self.personagem.y_vel = self.personagem.jump_alt
+
 
         self.personagem.update()
 
@@ -97,22 +98,26 @@ class Tela:
 class Personagem:
     def __init__(self, window, pos,assets):
         self.vida = 3
-        self.velocidade = [0,0]
+        self.jump_alt = 20
+        self.y_vel = self.jump_alt
+        self.velocidade = [0,self.y_vel]
         self.rect = pygame.Rect(pos[0], pos[1], 80, 100)
         self.window = window
         self.assets = assets
-        self
-    
+
+
     def desenha(self):
         self.rect.y += self.assets['gravidade']
         self.window.blit(self.assets['toshi'],(self.rect.x, self.rect.y))
     
     def update(self):
         self.rect.x += self.velocidade[0]*self.assets['tempo']
-        self.rect.y += self.velocidade[1]*self.assets['tempo']
 
     def muda_posicao(self):
         if self.rect.x >= 540:
             self.rect.x = 1
         if self.rect.x <= 0:
             self.rect.x = 530
+        if self.rect.y >= 600:
+            self.rect.y = 599
+        
