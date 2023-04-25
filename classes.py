cfb7e815a72dde26e2705bc7b0ead513e827c4bd
Henrique de Mendonça Junqueira Franco <ikejunqueira@gmail.com>
2023-04-25 14:57:54 -0300

    plataformas com colisao

--- classes.py
+++ classes.py
@@ -13,13 +13,16 @@ class Tela:
         imagem_toshi = pygame.transform.scale(pygame.image.load('assets/toshi.png').convert_alpha(),(80,100))
         botao_play = pygame.transform.scale(pygame.image.load('assets/botao_play.png'),(220,140))
         botao_info = pygame.transform.scale(pygame.image.load('assets/botao_info.png'),(220,140))
-        plataforma = pygame.transform.scale(pygame.image.load('assets/plataforma.png'),(110,70))
+        plataforma = pygame.transform.scale(pygame.image.load('assets/plataforma.png'),(80,20))
 
         self.state = {
             'tela_inicio':True,
             'tela_jogo':False,
             'tela_gameover': False,
-            'plataformas': []
+            'desce_tela': False,
+            'desce_tela_num':0,
+            'plataformas': [],
+            'rect_plataformas':[]
 
         }
 
@@ -35,6 +38,12 @@ class Tela:
             'gravidade': 1,
             'pulando': False
         }
+
+        with open('plataformas.txt','r') as arquivo:
+            linhas = arquivo.readlines()
+            for linha in linhas:
+                coord = linha.split(',')
+                self.state['plataformas'].append(pygame.Rect(int(coord[0]),int(coord[1]),80,15))
 
         self.personagem = Personagem(self.window, [230, 400] , self.assets)
     
@@ -60,11 +69,16 @@ class Tela:
                     self.personagem.velocidade[0] +=1000
                 if event.key == pygame.K_SPACE:
                     self.assets['pulando'] = True
+                if event.key == pygame.K_w:
+                    self.state['desce_tela'] = True
+
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_a:
                     self.personagem.velocidade[0] +=1000
                 if event.key == pygame.K_d:
                     self.personagem.velocidade[0] -=1000
+                if event.key == pygame.K_w:
+                    self.state['desce_tela'] = False
             if event.type == pygame.QUIT:
                 return False
        
@@ -76,7 +90,15 @@ class Tela:
                 self.personagem.y_vel = self.personagem.jump_alt
 
 
+        if self.state['desce_tela']:
+            for coord in self.state['plataformas']:
+                coord.y+=4
+
         self.personagem.update()
+        
+        for plataforma in self.state['plataformas']:
+            if plataforma.colliderect(self.personagem.rect):
+                print("oi")
 
         self.personagem.muda_posicao()
 
@@ -93,22 +115,10 @@ class Tela:
         if self.state['tela_jogo']:
             self.window.blit(self.assets['imagem_fundo'],(0,0))
 
-            # plataforma1 = Plataformas(self.window,110,70,100,100)
-            # plataforma2 = Plataformas(self.window,110,70,250,150)
-            # plataforma3 = Plataformas(self.window,110,70,260,350)
-            # plataforma4 = Plataformas(self.window,110,70,356,530)
-            # plataforma5 = Plataformas(self.window,110,70,324,465)
-            # plataforma6 = Plataformas(self.window,110,70,234,523)
-            # plataforma7 = Plataformas(self.window,110,70,74,321)
-            # plataforma8 = Plataformas(self.window,110,70,54,234)
-  
-            # plataforma1.desenha_plataformas()
-            # plataforma2.desenha_plataformas()
-            # plataforma3.desenha_plataformas()
-            # plataforma4.desenha_plataformas()
-            # plataforma5.desenha_plataformas()
-            # plataforma6.desenha_plataformas()
-            # plataforma7.desenha_plataformas()
+            for coord in self.state['plataformas']:
+                plat = Plataformas(self.window,80,15,coord.x,coord.y)
+                plat.desenha_plataformas()
+ 
             self.personagem.desenha()
             pygame.display.update()
         
@@ -150,9 +160,9 @@ class Plataformas:
         self.height = height
         self.x = coord_x
         self.y = coord_y
+        
 
     def desenha_plataformas(self):
         plataforma = pygame.transform.scale(pygame.image.load('assets/plataforma.png'),(self.width,self.height))
-        pygame.Rect(self.x,self.y,self.width,self.height)
         self.window.blit(plataforma,(self.x,self.y))
   
